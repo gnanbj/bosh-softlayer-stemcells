194e20285313cdedf08718a3fdcca6ef41589c34
jianqiu <wangjq@cn.ibm.com>
2015-11-23 00:20:49 +0800

    debug

--- main/main.go
+++ main/main.go
@@ -153,7 +153,7 @@ func init() {
 	flag.StringVar(&options.VersionFlag, "version", "", "the light stemcell version")
 	flag.StringVar(&options.StemcellInfoFilenameFlag, "stemcell-info-filename", "", "the path and filename to a JSON file containing the ID & UUID for a SoftLayer stemcell ")
 	flag.StringVar(&options.InfrastructureFlag, "infrastructure", "softlayer", "the light stemcell infrastructure, defaults to softlayer")
-	flag.StringVar(&options.HypervisorFlag, "hypervisor", "esxi", "the light stemcell version")
+	flag.StringVar(&options.HypervisorFlag, "hypervisor", "hyperv", "the light stemcell version")
 	flag.StringVar(&options.OsNameFlag, "os-name", "ubuntu-trusty", "the name of the operating system")
 }
 
